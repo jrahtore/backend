ff9f236e6b65789045f1b9b0e2a16a96e395fadc
Jitendra <jitendra.r3@gmail.com>
2018-06-24 13:05:13 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+export const facebook = {
+  clientID: '191791104636626',
+  clientSecret: 'baeec9b011ddef4b7b01df9abb970c20',
+  callbackURL: 'http://localhost:8081/auth/facebook/callback',
+  profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+};
+
+export const google = {
+  clientID: '42646943568-4q2m0s43kv92hdstd2fr8kg0ukegquhu.apps.googleusercontent.com',
+  clientSecret: 'ZpKelj_7JZ-o9R2hE4R5nzIF',
+  callbackURL: 'http://localhost:8081/auth/google/callback',
+};
